3651d6c3565247982ec1ac83774c832ee7eba83c
Sultan Majed <sultan.majed@proton.me>
2025-12-18 20:56:54 +0300

    make the hero section as neofech command output

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
-<script>
+<script lang="ts">
 	import Page from "./demo/+page.svelte";
+
+  let age: number = null;
+
+  let currentDate = new Date();
+  let DoB = new Date("Aug 14 2005");
+
+  age = currentDate.getFullYear() - DoB.getFullYear();
+
+  if (currentDate.getMonth() < DoB.getMonth()) {
+    age--;
+  }
 
 </script>
 <style>
@@ -19,14 +30,75 @@
     margin-top: 3rem;
     font-size: 0.9rem;
   }
+  td {
+    padding-left: 10px;
+    min-width: 120px;
+  }
 </style>
 
-<h1>It's me. Abu_jandal </h1>
+<h1> It's me. Abu_jandal</h1>
 
-<p class="mt-4">
-  <span class="icon"></span> Cybersecurity & Networking student<br />
-  <span class="icon"></span> Arch Linux • Hyprland • Neovim, joyer
-</p>
+<div class="bg-gruvbox-dark0 py-10 sm:px-0 px-5 flex sm:flex-row flex-col items-center">
+  <div class="sm:min-w-70 flex justify-center items-center">
+    <pre class="ascii-art">
+  ////^\\\\
+  | ^   ^ |
+ @:(o) (o):@
+  |   &gt;   |
+  |  ___  |
+   \_____/
+ ____|  |____
+/    \__/    \
+    </pre>
+  </div>
+  <div class="">
+    <div class="pl-1 mb-5 border-l-2 border-gruvbox-blue rounded-l-sm">
+      <table>
+        <tbody>
+          <tr>
+            <td><span class="icon"></span> Name:</td>
+            <td> Sultan Majed</td>
+          </tr>
+          <tr>
+            <td><span class="icon">󰻗</span> Age:</td>
+            <td> {age} years old</td>
+          </tr>
+          <tr>
+            <td><span class="icon"></span> Major:</td>
+            <td> Cybersecurity</td>
+          </tr>
+          <tr>
+            <td><span class="icon"></span> Langs:</td>
+            <td>en, ara</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div class="pl-1 border-l-2 border-gruvbox-blue rounded-l-sm">
+      <table>
+        <tbody>
+          <tr>
+            <td><span class="icon"></span> Distro:</td>
+            <td>Arch Linux</td>
+          </tr>
+          <tr>
+            <td><span class="icon"></span> WM:</td>
+            <td>Hyprland</td>
+          </tr>
+          <tr>
+            <td><span class="icon"></span> Shell:</td>
+            <td>Fish</td>
+          </tr>
+          <tr>
+            <td><span class="icon"></span> Term:</td>
+            <td>Kitty</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
 
 <h2><span class="icon">󰞋</span> About Me</h2>
 <p class="pl-2">
@@ -36,13 +108,23 @@
   to network protocols — and applying that knowledge in practical projects.
 </p>
 
-<h2><span class="icon">󰈙</span> Programming Languages</h2>
+<h2><span class="icon">󰘳</span> Programming Languages</h2>
 <div class="grid">
   <div class="card"><span class="icon"></span> Rust</div>
   <div class="card"><span class="icon"></span> Zig (0.15)</div>
   <div class="card"><span class="icon"></span> JavaScript</div>
-  <div class="card"><span class="icon"></span> Lua (basic)</div>
-  <div class="card"><span class="icon"></span> Python (basic)</div>
+  <div class="card"><span class="icon"></span> Lua</div>
+  <div class="card"><span class="icon"></span> Python</div>
+  <div class="card"><span class="icon"></span> PHP</div>
+  <div class="card"><span class="icon"></span> Java</div>
+  <div class="card"><span class="icon"></span> C#</div>
+</div>
+
+<h2><span class="icon"></span> Markup Languages</h2>
+<div class="grid">
+  <div class="card"><span class="icon"></span> HTML & CSS</div>
+  <div class="card"><span class="icon"></span> Markdown</div>
+  <div class="card"><span class="icon"></span> LaTeX</div>
 </div>
 
 <h2><span class="icon">󰏓</span> Frontend & Frameworks</h2>
@@ -68,10 +150,10 @@
 
 <h2><span class="icon">󰆍</span> Environment</h2>
 <div class="grid">
-  <div class="card"><span class="icon"></span> Arch Linux (Maniual)</div>
-  <div class="card"><span class="icon"></span> Hyprland (Wayland)</div>
+  <div class="card"><span class="icon"></span> Arch Linux</div>
+  <div class="card"><span class="icon"></span> Hyprland</div>
   <div class="card"><span class="icon"></span> Neovim (NvChad)</div>
-  <div class="card"><span class="icon">󰙀</span> Tmux (kitty)</div>
+  <div class="card"><span class="icon">󰙀</span> Tmux</div>
   <div class="card"><span class="icon"></span> CaskaydiaCove (Nerd Font)</div>
 </div>
 
@@ -83,8 +165,8 @@
 
 <h2><span class="icon">󰍡</span> Connect</h2>
 <div class="pl-2">
-  <p> GitHub: <a href="https://github.com/ex-jandal" target="_blank">@ex-jandal</a></p>
-  <p>󰇮 Email: <a href="mailto:sultan.majed@proton.me">Sultan Majed</a></p>
+  <p><span class="icon"></span> GitHub: <a href="https://github.com/ex-jandal" target="_blank">@ex-jandal</a></p>
+  <p><span class="icon">󰇮</span> Email: <a href="mailto:sultan.majed@proton.me">Sultan Majed</a></p>
 </div>
 
 <footer>
