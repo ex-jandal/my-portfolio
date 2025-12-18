731dbaff1f12ecbe78aa8c225df0d513f678f8e1
Sultan Majed <sultan.majed@proton.me>
2025-12-18 15:50:32 +0300

    fix prompt wrapping

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,26 +10,22 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<style>
-  h1 {
-    font-size: var(--text-xl);
-  }
-</style>
-
 <div class="max-w-257 m-auto">
-  <header class="bg-gruvbox-dark3 shadow-gruvbox-bright-orange shadow-md text-gruvbox-bright-orange m-5">
+  <header class="bg-gruvbox-dark3 shadow-gruvbox-bright-orange shadow-md text-gruvbox-bright-orange m-1">
     <nav class="flex flex-row justify-between items-center pl-2 pr-4 py-2">
       <div class="flex flex-row items-center gap-2">
-        <img src="{favicon256}" class="h-15 pr-2 border-r border-gruvbox-green" alt="">
+          <img src="{favicon256}" class="h-12 hidden sm:inline pr-2 border-r border-gruvbox-green" alt="">
         <!-- <h1>It's me. Abu_jandal </h1> -->
 
-        <div class="text-xl">
+        <div class="text-lg">
           <div class="align-middle">
-            <span class="text-gruvbox-bright-yellow text-[23.6px] bg-none"></span><span class="bg-gruvbox-bright-yellow text-gruvbox-dark0">󰪢 22s</span><span class="text-gruvbox-bright-yellow text-[23.6px] bg-none"></span><span class="text-gruvbox-light0">•</span><span class="text-gruvbox-bright-yellow text-[23.6px] bg-none"></span><span class="bg-gruvbox-bright-yellow text-gruvbox-dark0">󰉋  ../abu_jandal</span><span class="text-gruvbox-bright-yellow text-[23.6px] bg-none"></span><br/>
+            <span class="text-gruvbox-bright-yellow text-[21px] bg-none"></span><span class="bg-gruvbox-bright-yellow text-gruvbox-dark0">󰪢 21s</span><span class="text-gruvbox-bright-yellow text-[21px] bg-none"></span><span class="text-gruvbox-light0">•</span><span class="text-gruvbox-bright-yellow text-[21px] bg-none"></span><span class="bg-gruvbox-bright-yellow text-gruvbox-dark0">󰉋  ../abu_jandal</span><span class="text-gruvbox-bright-yellow text-[21px] bg-none"></span><br/>
           </div>
 
           <div class="ml-4">
-            <span class="text-gruvbox-bright-green"> letsfetch <span></span> </span>
+            <span class="text-gruvbox-bright-green">
+              <img src="{favicon256}" class="h-6 inline sm:hidden pr-2 border-r border-gruvbox-green" alt="">
+               letsfetch</span>
           </div>
         </div>
       </div>
