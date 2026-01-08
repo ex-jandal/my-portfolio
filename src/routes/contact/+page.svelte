88b0743072037f2b8f26aedbc6060082f4c1128e
Sultan Majed <sultan.majed@proton.me>
2026-01-08 19:45:39 +0300

    fix: contact links method

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -11,9 +11,9 @@
   <h2 class="frist-h2"><span class="icon">󰍡</span> Connect</h2>
   <div class="pl-2">
     <p><span class="icon"></span> GitHub: <a href="https://github.com/ex-jandal" target="_blank">@ex-jandal</a></p>
-    <p><span class="icon"></span> Discord <a href="https://discord.com/users/884869855042490528">@ex.jandal</a></p>
-    <p><span class="icon"></span> Telegram <a href="https://t.me/ab0_jandal">@ab0_jandal</a></p>
-    <p><span class="icon">󰇮</span> Email: <a href="mailto:sultan.majed@proton.me">sultan.majed@proton.me</a></p>
+    <p><span class="icon"></span> Discord <a href="https://discord.com/users/884869855042490528" target="_blank">@ex.jandal</a></p>
+    <p><span class="icon"></span> Telegram <a href="https://t.me/ab0_jandal" target="_blank">@ab0_jandal</a></p>
+    <p><span class="icon">󰇮</span> Email: <a href="mailto:sultan.majed@proton.me" target="_blank">sultan.majed@proton.me</a></p>
     <!-- <p><span class="icon">󰏲</span> Phone: <a href="tel:+967782424366">+967 78-2424-366</a></p> -->
   </div>
 </div>
