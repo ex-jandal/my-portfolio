338b765395f39f74b414af1998087b26448ae7c8
Sultan Majed <sultan.majed@proton.me>
2026-04-10 17:07:41 +0300

    swap: the adoptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// import adapterNetlify from '@sveltejs/adapter-netlify';
 
 
 /** @type {import('@sveltejs/kit').Config} */
